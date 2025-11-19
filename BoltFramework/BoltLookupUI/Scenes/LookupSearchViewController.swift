BoltDocs/BoltOSS

commit f89106f5d3645a965f23c76aa1db3fe7049819fc

Do not dismiss search when interacting with the lookup list.

BoltFramework/BoltLookupUI/Scenes/LookupSearchViewController.swift
@@ -87,7 +87,7 @@ public final class LookupSearchController: UISearchController, HasDisposeBag {
       }
       .disposed(by: disposeBag)
 
-    state.presentsLookupList
+    state.presentsLookupListDriver
       .drive(rx.showsSearchResultsController)
       .disposed(by: disposeBag)
 
@@ -161,7 +161,9 @@ extension LookupSearchController: UITextFieldDelegate {
   }
 
   public func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
-    dismiss(animated: true)
+    if !state.presentsLookupList {
+      dismiss(animated: true)
+    }
   }
 
 }

BoltFramework/BoltLookupUI/Scenes/Content/Lists/ViewModels/LookupAllEnteriesViewModel.swift
@@ -84,7 +84,7 @@ final class LookupAllEntriesViewModel: LookupListViewModel {
 
     Observable.combineLatest(
       routingState.searchQuery.asObservable(),
-      routingState.presentsLookupList.asObservable()
+      routingState.presentsLookupListDriver.asObservable()
     )
     .filter { _, presentsLookupList in
       return presentsLookupList

BoltFramework/BoltLookupUI/Scenes/Content/Lists/ViewModels/LookupTypeFilteringViewModel.swift
@@ -81,7 +81,7 @@ final class LookupTypeFilteringViewModel: LookupListViewModel {
 
     Observable.combineLatest(
       routingState.searchQuery.asObservable(),
-      routingState.presentsLookupList.asObservable()
+      routingState.presentsLookupListDriver.asObservable()
     )
     .filter { _, presentsLookupList in
       return presentsLookupList

BoltFramework/BoltLookupUI/States/LookupRoutingState.swift
@@ -79,11 +79,12 @@ final class LookupRoutingState: HasDisposeBag {
   private let searchQueryRelay = BehaviorRelay<String>(value: "")
   lazy var searchQuery: Driver<String> = { searchQueryRelay.asDriver() }()
 
-  lazy var presentsLookupList: Driver<Bool> = {
+  lazy var presentsLookupListDriver: Driver<Bool> = {
     return showsDocumentationPage
       .asDriver()
       .map(!)
   }()
+  var presentsLookupList: Bool { !(showsDocumentationPage.value) }
 
   private let showsDocumentationPage = BehaviorRelay<Bool>(value: false)
 

commit ebdcb27a2bcba76bdc617856a7c3320ab9247875

Fix lookup search field text and tokens not preserved on cancel.
Fixes the regression introduced in f89106f.

This issue happens due to `textFieldDidEndEditing(_:reason:)` shadows `textFieldDidEndEditing(_:)`.

BoltFramework/BoltLookupUI/Scenes/LookupSearchViewController.swift
@@ -121,6 +121,29 @@ public final class LookupSearchController: UISearchController, HasDisposeBag {
 
 }
 
+// MARK: Search Field Text Preservation
+
+private extension LookupSearchController {
+
+  private func preserveSearchFieldText() {
+    preservedSearchFieldText = SearchTextFieldTextPreservation(
+      text: searchBar.searchTextField.text,
+      tokens: searchBar.searchTextField.tokens
+    )
+  }
+
+  private func restoreSearchFieldText() {
+    guard let preservedSearchFieldText = self.preservedSearchFieldText else {
+      return
+    }
+    searchBar.searchTextField.text = preservedSearchFieldText.text
+    searchBar.searchTextField.tokens = preservedSearchFieldText.tokens
+    searchBar.delegate?.searchBar?(searchBar, textDidChange: searchBar.text ?? "")
+    self.preservedSearchFieldText = nil
+  }
+
+}
+
 extension LookupSearchController: UITextFieldDelegate {
 
   public func textFieldDidChangeSelection(_ textField: UITextField) {
@@ -160,12 +183,6 @@ extension LookupSearchController: UITextFieldDelegate {
     return true
   }
 
-  public func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
-    if !state.presentsLookupList {
-      dismiss(animated: true)
-    }
-  }
-
 }
 
 extension LookupSearchController: UISearchBarDelegate {
@@ -179,24 +196,10 @@ extension LookupSearchController: UISearchBarDelegate {
   }
 
   public func textFieldDidEndEditing(_ textField: UITextField) {
-    restoreSearchFieldText()
-  }
-
-  private func preserveSearchFieldText() {
-    preservedSearchFieldText = SearchTextFieldTextPreservation(
-      text: searchBar.searchTextField.text,
-      tokens: searchBar.searchTextField.tokens
-    )
-  }
-
-  private func restoreSearchFieldText() {
-    guard let preservedSearchFieldText = self.preservedSearchFieldText else {
-      return
+    if !state.presentsLookupList {
+      dismiss(animated: true)
     }
-    searchBar.searchTextField.text = preservedSearchFieldText.text
-    searchBar.searchTextField.tokens = preservedSearchFieldText.tokens
-    searchBar.delegate?.searchBar?(searchBar, textDidChange: searchBar.text ?? "")
-    self.preservedSearchFieldText = nil
+    restoreSearchFieldText()
   }
 
 }